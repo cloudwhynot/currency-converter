e52d9587f6a216d9c0b5dd1d61e9e1e98fc3e3a7
Abdul Khok <khok.abdul@gmail.com>
2024-03-02 15:08:54 +0200

    feat: added currency conversion functionality and result display

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ currencyCodes.forEach((currency) => {
         dropdown.add(option);
     });
 });
+
+fromDropDown.value = "USD";
+toDropDown.value = "UAH";
+
+let convertCurrency = () => {
+    const amount = document.querySelector("#amount").value;
+    const fromCurrency = fromDropDown.value;
+    const toCurrency = toDropDown.value;
+
+    if(amount) {
+        fetch(api)
+        .then(resp => resp.json())
+        .then(data => {
+            let fromExchangeRate = data.conversion_rates[fromCurrency];
+            let toExchangeRate = data.conversion_rates[toCurrency];
+            const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+            result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+        });
+    } else {
+        alert("Please enter the amount");
+    }
+};
+
+document.querySelector("#convert-button").addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
